4f4c63d0fc0540635f714749ec7273d9caa447ee
markstownsend <markstownsend@yahoo.com>
2015-09-14 10:11:38 +0100

    rearranged solution, added build script

---
+++ build.fsx
@@ -0,0 +1,15 @@
+﻿// include FAKE libs
+#r "packages/FAKE.4.4.1/tools/FakeLib.dll"
+
+open Fake
+open Fake.Testing
+
+// directories
+let testDir = "test/LastCellTests/bin/Debug/"
+
+Target "xUnitTest" (fun _ ->
+    !! (testDir @@ "*Tests.dll")
+        |> xUnit2 (fun p -> {p with HtmlOutputPath = Some(testDir @@ ".xunit.html")})
+)
+
+RunTargetOrDefault "xUnitTest"
